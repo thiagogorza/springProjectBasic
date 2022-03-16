c9799d441dd7bf2f3dd5ccbce6527debe2f52599
Thiago Gorza <Thiago Gorza>
2022-03-16 16:18:23 -0300

    Initial commit

---
+++ src/main/java/com/thiagodev/springprojectbasic/SpringProjectBasicApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodev.springprojectbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thiagodev/springprojectbasic/SpringProjectBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodev.springprojectbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
