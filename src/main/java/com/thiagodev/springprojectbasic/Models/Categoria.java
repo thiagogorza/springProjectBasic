1008a83c7efdfadeec3dfa8d45cd84ced0abf725
Thiago Gorza <Thiago Gorza>
2022-03-18 13:55:57 -0300

    Banco h2 configurado

--- src/main/java/com/thiagodev/springprojectbasic/Models/Categoria.java
+++ src/main/java/com/thiagodev/springprojectbasic/Models/Categoria.java
@@ -2,10 +2,7 @@ package com.thiagodev.springprojectbasic.Models;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
---
+++ src/main/java/com/thiagodev/springprojectbasic/resources/CategoriaResource.java
@@ -0,0 +1,9 @@
+package com.thiagodev.springprojectbasic.resources;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "categorias")
+public class CategoriaResource {
+
+}
