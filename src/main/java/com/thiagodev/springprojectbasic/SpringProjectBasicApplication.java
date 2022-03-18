3287a5af011d75c4f53d6c637e180841f325f3bc
Thiago Gorza <Thiago Gorza>
2022-03-18 14:41:17 -0300

    Criando operecao de instanciacao

--- src/main/java/com/thiagodev/springprojectbasic/SpringProjectBasicApplication.java
+++ src/main/java/com/thiagodev/springprojectbasic/SpringProjectBasicApplication.java
@@ -1,13 +1,30 @@
 package com.thiagodev.springprojectbasic;
 
+import com.thiagodev.springprojectbasic.Models.Categoria;
+import com.thiagodev.springprojectbasic.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringProjectBasicApplication {
+public class SpringProjectBasicApplication implements CommandLineRunner {
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringProjectBasicApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Categoria cat1 = new Categoria(null,"Eletronicos");
+        Categoria cat2 = new Categoria(null,"Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
