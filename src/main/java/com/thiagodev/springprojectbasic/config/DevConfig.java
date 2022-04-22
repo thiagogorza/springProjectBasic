8fe6337a611b88f5c36276d7e78f572f03fc080f
Thiago Gorza <Thiago Gorza>
2022-04-22 11:47:10 -0300

    Criando profile de Desenvolvimento

---
+++ src/main/java/com/thiagodev/springprojectbasic/config/DevConfig.java
@@ -0,0 +1,36 @@
+package com.thiagodev.springprojectbasic.config;
+
+import com.thiagodev.springprojectbasic.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+
+    @Bean
+    public boolean instantiateDataBase() throws ParseException {
+
+        if(!strategy.equals("create")){
+            return false;
+        }
+
+        dbService.instantiateTestDataBase();
+
+        return true;
+    }
+
+
+}
