0082141c650de8dd86cebd221b0b248b2e891c7c
Thiago Gorza <Thiago Gorza>
2022-04-27 11:50:03 -0300

    mudando banco de producao para postgres

--- src/main/java/com/thiagodev/springprojectbasic/config/DevConfig.java
+++ src/main/java/com/thiagodev/springprojectbasic/config/DevConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("dev")
+@Profile("heroku")
 public class DevConfig {
 
     @Autowired
