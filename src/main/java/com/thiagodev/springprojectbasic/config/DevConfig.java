72d91f72ec3ceffa1a43473d4a9b1ac99ce2edc2
Thiago Gorza <Thiago Gorza>
2022-04-27 12:26:57 -0300

    criacao de tabelas do banco de dados feita no heroku com propriedades comentadas em dev

--- src/main/java/com/thiagodev/springprojectbasic/config/DevConfig.java
+++ src/main/java/com/thiagodev/springprojectbasic/config/DevConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("heroku")
+@Profile("dev")
 public class DevConfig {
 
     @Autowired
