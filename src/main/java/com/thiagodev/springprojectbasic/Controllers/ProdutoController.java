c0cbe2c95f4719d4e9bd6f9ab5bd69205b13c5e7
thiagogorza <thiagomgorza@gmail.com>
2022-05-07 17:00:12 -0300

    configuracao inicial do spring security

--- src/main/java/com/thiagodev/springprojectbasic/Controllers/ProdutoController.java
+++ src/main/java/com/thiagodev/springprojectbasic/Controllers/ProdutoController.java
@@ -14,6 +14,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -33,11 +34,11 @@ public class ProdutoController {
     }
 
     @GetMapping(value = "{id}")
-    public ResponseEntity<?> findById(Integer id) {
+    public ResponseEntity<Produto> findById(@PathVariable Integer id) {
 
         Produto obj = produtoService.findByid(id);
 
-        return ResponseEntity.ok(obj);
+        return ResponseEntity.ok().body(obj);
 
     }
 
--- src/main/java/com/thiagodev/springprojectbasic/config/SecurityConfig.java
+++ src/main/java/com/thiagodev/springprojectbasic/config/SecurityConfig.java
@@ -1,17 +1,48 @@
 package com.thiagodev.springprojectbasic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private Environment env;
 
     private static final String[] PUBLIC_MATCHERS = {
-      "/h2-console/**",
-      "/produtos/**",
-      "/categorias/**"
-    };
+      "/h2-console/**"
 
+    };
+    private static final String[] PUBLIC_MATCHERS_GET = { //permissao apenas para de acesso apenas para leitura
+            "/produtos/**",
+            "/categorias/**"
+    };
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+        http.cors().and().csrf().disable(); // desabilita a protecao contra ataques csrf (como usuario nao tera sessao n é necessario)
+        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
+                .antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //nao cria sessao de usuario
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
